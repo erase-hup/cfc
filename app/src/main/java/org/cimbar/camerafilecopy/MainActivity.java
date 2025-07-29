9da9fe4553869e0156420c016b5e5226053a99fe
erase-hup <yasin.emrebas@gmail.com>
2025-07-29 16:20:41 +0300

    Update MainActivity.java

--- app/src/main/java/org/cimbar/camerafilecopy/MainActivity.java
+++ app/src/main/java/org/cimbar/camerafilecopy/MainActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -17,6 +19,7 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
+import androidx.documentfile.provider.DocumentFile;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -35,6 +38,10 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     private static final String TAG = "MainActivity";
     private static final int CAMERA_PERMISSION_REQUEST = 1;
     private static final int CREATE_FILE = 11;
+    private static final int SELECT_DIRECTORY = 12;
+    
+    private static final String PREFS_NAME = "CimbarPrefs";
+    private static final String PREF_SAVE_DIRECTORY = "save_directory";
 
     private CameraBridgeViewBase mOpenCvCameraView;
     private ToggleButton mModeSwitch;
@@ -42,6 +49,9 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     private int detectedMode = 68;
     private String dataPath;
     private String activePath;
+    
+    // Kullanıcının seçtiği dizini saklamak için
+    private Uri savedDirectoryUri;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -76,6 +86,9 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
 
         this.dataPath = this.getFilesDir().getPath();
 
+        // Kaydedilmiş dizini yükle
+        loadSavedDirectory();
+
         setContentView(R.layout.activity_main);
         mOpenCvCameraView = findViewById(R.id.main_surface);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
@@ -91,6 +104,44 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
                 }
             }
         });
+    }
+
+    private void loadSavedDirectory() {
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        String uriString = prefs.getString(PREF_SAVE_DIRECTORY, null);
+        if (uriString != null) {
+            savedDirectoryUri = Uri.parse(uriString);
+            // Persistable URI permission kontrolü
+            try {
+                getContentResolver().takePersistableUriPermission(
+                    savedDirectoryUri, 
+                    Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+                );
+            } catch (SecurityException e) {
+                Log.w(TAG, "Cannot take persistable URI permission, will prompt user again");
+                savedDirectoryUri = null;
+            }
+        }
+    }
+
+    private void saveDirectory(Uri uri) {
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PREF_SAVE_DIRECTORY, uri.toString());
+        editor.apply();
+        savedDirectoryUri = uri;
+    }
+
+    private void promptForDirectory() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+        startActivityForResult(intent, SELECT_DIRECTORY);
+    }
+
+    private String addZipExtension(String filename) {
+        if (!filename.toLowerCase().endsWith(".zip")) {
+            return filename + ".zip";
+        }
+        return filename;
     }
 
     @Override
@@ -180,46 +231,107 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
                     }
                 });
             }
-
         }
         else if (!res.isEmpty()) {
-            Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
-            intent.addCategory(Intent.CATEGORY_OPENABLE);
-            intent.setType("application/octet-stream");
-            intent.putExtra(Intent.EXTRA_TITLE, res);
-            // can't get putExtra to work for extra values, so we'll save it in the class
+            // Dosya adına .zip uzantısı ekle
+            String filename = addZipExtension(res);
             this.activePath = this.dataPath + "/" + res;
-            startActivityForResult(intent, CREATE_FILE);
+            
+            // Eğer kaydedilmiş dizin varsa, doğrudan oraya kaydet
+            if (savedDirectoryUri != null) {
+                saveToDirectory(filename);
+            } else {
+                // Kullanıcıdan dizin seçmesini iste
+                promptForDirectory();
+            }
         }
 
         // return processed frame for live preview
         return mat;
     }
 
+    private void saveToDirectory(String filename) {
+        try {
+            DocumentFile directory = DocumentFile.fromTreeUri(this, savedDirectoryUri);
+            if (directory != null && directory.exists()) {
+                DocumentFile newFile = directory.createFile("application/octet-stream", filename);
+                if (newFile != null) {
+                    copyFileToUri(newFile.getUri());
+                } else {
+                    Log.e(TAG, "Cannot create file in selected directory");
+                    Toast.makeText(this, "Dosya oluşturulamadı, lütfen farklı bir dizin seçin", Toast.LENGTH_LONG).show();
+                    promptForDirectory();
+                }
+            } else {
+                Log.e(TAG, "Selected directory no longer exists");
+                Toast.makeText(this, "Seçilen dizin artık mevcut değil, yeni bir dizin seçin", Toast.LENGTH_LONG).show();
+                savedDirectoryUri = null;
+                promptForDirectory();
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error saving to directory: " + e.toString());
+            Toast.makeText(this, "Dosya kaydedilemedi: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void copyFileToUri(Uri destinationUri) {
+        try (
+                InputStream istream = new FileInputStream(this.activePath);
+                OutputStream ostream = getContentResolver().openOutputStream(destinationUri)
+        ) {
+            byte[] buf = new byte[8192];
+            int length;
+            while ((length = istream.read(buf)) > 0) {
+                ostream.write(buf, 0, length);
+            }
+            ostream.flush();
+            
+            Toast.makeText(this, "Dosya başarıyla kaydedildi!", Toast.LENGTH_SHORT).show();
+            Log.i(TAG, "File saved successfully to: " + destinationUri.toString());
+            
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to write file: " + e.toString());
+            Toast.makeText(this, "Dosya kaydedilemedi: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        } finally {
+            try {
+                new File(this.activePath).delete();
+            } catch (Exception e) {
+                Log.w(TAG, "Could not delete temp file: " + e.toString());
+            }
+            this.activePath = null;
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (resultCode == RESULT_OK && requestCode == CREATE_FILE) {
-            if (this.activePath == null)
-                return;
-
-            // copy this.activePath (tempfile) to the user-specified location
-            try (
-                    InputStream istream = new FileInputStream(this.activePath);
-                    OutputStream ostream = getContentResolver().openOutputStream(data.getData())
-            ) {
-                byte[] buf = new byte[8192];
-                int length;
-                while ((length = istream.read(buf)) > 0) {
-                    ostream.write(buf, 0, length);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_DIRECTORY && data != null) {
+                Uri uri = data.getData();
+                if (uri != null) {
+                    // Persistable URI permission al
+                    try {
+                        getContentResolver().takePersistableUriPermission(
+                            uri, 
+                            Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+                        );
+                        saveDirectory(uri);
+                        
+                        // Eğer bekleyen bir dosya varsa, şimdi kaydet
+                        if (this.activePath != null) {
+                            String filename = new File(this.activePath).getName();
+                            filename = addZipExtension(filename);
+                            saveToDirectory(filename);
+                        }
+                    } catch (SecurityException e) {
+                        Log.e(TAG, "Cannot take persistable URI permission: " + e.toString());
+                        Toast.makeText(this, "Dizin erişim izni alınamadı", Toast.LENGTH_LONG).show();
+                    }
                 }
-                ostream.flush();
-            } catch (Exception e) {
-                Log.e(TAG, "failed to write file " + e.toString());
-            } finally {
-                try {
-                    new File(this.activePath).delete();
-                } catch (Exception e) {}
-                this.activePath = null;
+            } else if (requestCode == CREATE_FILE) {
+                // Eski kod - artık kullanılmayacak ama geriye dönük uyumluluk için bırakılabilir
+                if (this.activePath != null && data != null) {
+                    copyFileToUri(data.getData());
+                }
             }
         }
     }
@@ -227,4 +339,3 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     private native String processImageJNI(long mat, String path, int modeInt);
     private native void shutdownJNI();
 }
-
